164d07eee8f81dc920e2f176356ae54d792b10bb
越智 修司 <ponpoko1968@gmail.com>
2014-08-08 10:41:36 +0900

    コマンドラインで起動してからの一連の動作を記述

--- lib/libra_reminder/controller.rb
+++ lib/libra_reminder/controller.rb
@@ -1,15 +1,61 @@
 # coding: utf-8
+require 'optparse'
 
 module LibraReminder
 
   class Controller
-
-    def initialize(config)
-      @config = config
+    def self.run(argv)
+      new(argv).execute
     end
 
+    def initialize(argv)
+      @argv = argv
+    end
+
+    def execute
+      options = {}
+      parser = create_option_parser(options)
+      parser.parse!(@argv)
+      if options[:path_to_config]
+        path_to_config = options[:path_to_config]
+      else
+        path_to_config = File.join(ENV['HOME'], '.libra_reminder', 'config.yaml')
+      end
+      @config = YAML.load_file(path_to_config)
+
+      DB.prepare
+
+      books_on_web = books_from_library(@config)
+      consistent_with_db(books_on_web)
+      consistent_with_gcal
+    end
+
+    def create_option_parser(options)
+      OptionParser.new do |opt|
+        opt.banner = "Usage: #{opt.program_name} [-h|--help][-v|--version] [-c|--config <path-to-config.yaml>]"
+        opt.separator ''
+        opt.separator "#{opt.program_name} Availavle Commands:"
+        # help
+        opt.on_head('-c VAL','--config=VAL','specify path to config.yaml') do |v|
+          options[:path_to_config] = v
+        end
+        # help
+        opt.on_head('-h','--help','Show this message') do |v|
+          puts opt.help
+          exit
+        end
+        # version
+        opt.on_head('-v','--version','show program version') do |v|
+          opt.version = LibraReminder::VERSION
+          puts opt.version
+          exit
+        end
+      end
+    end
+
+
     def books_from_library(config)
-      Scraper.fetchBooksOnLoan()
+      Scraper.fetchBooksOnLoan(config)
     end
 
 
@@ -20,6 +66,7 @@ module LibraReminder
         if recs.empty?
           Book.create!(book_id: book[:book_id], book_name: book[:book_name], deadline: book[:deadline] )
         else
+          recs[0].deadline = book[:deadline]
           recs[0].to_be_deleted = false
           recs[0].save
         end
